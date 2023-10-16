56ab78c37bb25303346579cc44a66f1373cbe444
uYanki <2397410949@qq.com>
2023-10-17 02:05:40 +0800

    1

--- 4.Examples/std/board/enc/pulse.c
+++ 4.Examples/std/board/enc/pulse.c
@@ -3,50 +3,137 @@
 
 // #define TIM_FRQ            168000000ul  // 168M (APB2)
 
-#define PULSE_GEN_SLV_GPIO_CLKEN  RCC_AHB1PeriphClockCmd
-#define PULSE_GEN_SLV_GPIO_CLK    RCC_AHB1Periph_GPIOA
-#define PULSE_GEN_SLV_GPIO_PORT   GPIOA
-#define PULSE_GEN_SLV_GPIO_PIN    GPIO_Pin_6
-#define PULSE_GEN_SLV_GPIO_PINSRC GPIO_PinSource6
-#define PULSE_GEN_SLV_GPIO_AF     GPIO_AF_TIM13
+#define PULSE_GEN_MST_GPIO_CLKEN     RCC_AHB1PeriphClockCmd
+#define PULSE_GEN_MST_GPIO_CLK       RCC_AHB1Periph_GPIOA
+#define PULSE_GEN_MST_GPIO_PORT      GPIOA
+#define PULSE_GEN_MST_GPIO_PIN       GPIO_Pin_3
+#define PULSE_GEN_MST_GPIO_PINSRC    GPIO_PinSource3
+#define PULSE_GEN_MST_GPIO_AF        GPIO_AF_TIM2
 
-#define PULSE_GEN_MST_TIM_FRQ     84000000ul  // 84M (APB1)
-#define PULSE_GEN_MST_TIM_CLKEN   RCC_APB1PeriphClockCmd
-#define PULSE_GEN_MST_TIM_CLK     RCC_APB1Periph_TIM13
-#define PULSE_GEN_MST_TIM_PORT    TIM13
-#define PULSE_GEN_MST_TIM_CH      TIM_Channel_1
-#define PULSE_GEN_MST_TIM_OCxInit TIM_OC1Init
+#define PULSE_GEN_MST_TIM_FRQ        84000000ul  // 84M (APB1)
+#define PULSE_GEN_MST_TIM_CLKEN      RCC_APB1PeriphClockCmd
+#define PULSE_GEN_MST_TIM_CLK        RCC_APB1Periph_TIM2
+#define PULSE_GEN_MST_TIM_PORT       TIM2
+#define PULSE_GEN_MST_TIM_CH         TIM_Channel_4
+#define PULSE_GEN_MST_TIM_OCInit     TIM_OC4Init
 
-#define PULSE_GEN_SLV_TIM_FRQ     84000000ul  // 84M (APB1)
-#define PULSE_GEN_SLV_TIM_CLKEN   RCC_APB1PeriphClockCmd
-#define PULSE_GEN_SLV_TIM_CLK     RCC_APB1Periph_TIM13
-#define PULSE_GEN_SLV_TIM_PORT    TIM13
-#define PULSE_GEN_SLV_TIM_CH      TIM_Channel_1
-#define PULSE_GEN_SLV_TIM_OCxInit TIM_OC1Init
+#define PULSE_GEN_SLV_TIM_FRQ        84000000ul  // 84M (APB1)
+#define PULSE_GEN_SLV_TIM_CLKEN      RCC_APB1PeriphClockCmd
+#define PULSE_GEN_SLV_TIM_CLK        RCC_APB1Periph_TIM3
+#define PULSE_GEN_SLV_TIM_PORT       TIM3
+#define PULSE_GEN_SLV_TIM_ITR        TIM_TS_ITR1
+#define PULSE_GEN_SLV_TIM_IRQn       TIM3_IRQn
+#define PULSE_GEN_SLV_TIM_IRQHandler TIM3_IRQHandler
+#define PULSE_GEN_SLV_TIM_TRGSRC     TIM_TRGOSource_OC1Ref  //??
+#define PULSE_GEN_SLV_TIM_FLAG       TIM_FLAG_CC1
+#define PULSE_GEN_SLV_TIM_IT         TIM_IT_CC1
+
+// #define PULSE_GEN_SLV_TIM_TRGSRC     TIM_TRGOSource_OC4Ref  //??
+// #define PULSE_GEN_SLV_TIM_FLAG       TIM_FLAG_CC4
+// #define PULSE_GEN_SLV_TIM_IT         TIM_IT_CC4
 
 //
+
+void PULSE_GEN_SLV_TIM_IRQHandler()  // TIM3达到指定脉冲数回调函数
+{
+    if (SET == TIM_GetITStatus(PULSE_GEN_SLV_TIM_PORT, PULSE_GEN_SLV_TIM_IT))
+    {
+        TIM_CtrlPWMOutputs(PULSE_GEN_MST_TIM_PORT, DISABLE);
+
+        TIM_ITConfig(PULSE_GEN_SLV_TIM_PORT, PULSE_GEN_SLV_TIM_IT, DISABLE);
+        TIM_ClearITPendingBit(PULSE_GEN_SLV_TIM_PORT, PULSE_GEN_SLV_TIM_IT);
+
+        TIM_Cmd(PULSE_GEN_MST_TIM_PORT, DISABLE);
+        TIM_Cmd(PULSE_GEN_SLV_TIM_PORT, DISABLE);
+    }
+}
+
+void PulseGenN(u32 count)  // 产生 PWM
+{
+    TIM_SetAutoreload(PULSE_GEN_MST_TIM_PORT, count - 1);
+    TIM_ITConfig(PULSE_GEN_SLV_TIM_PORT, PULSE_GEN_SLV_TIM_IT, ENABLE);
+    TIM_CtrlPWMOutputs(PULSE_GEN_MST_TIM_PORT, ENABLE);
+
+    TIM_Cmd(PULSE_GEN_MST_TIM_PORT, ENABLE);
+    TIM_Cmd(PULSE_GEN_SLV_TIM_PORT, ENABLE);
+}
 
 void PulseGenInit(void)
 {
     // gpio
-//    {
-//        GPIO_InitTypeDef GPIO_InitStructure = {
-//            .GPIO_Pin   = PULSE_GEN_GPIO_PIN,
-//            .GPIO_Mode  = GPIO_Mode_AF,
-//            .GPIO_Speed = GPIO_Speed_100MHz,
-//            .GPIO_OType = GPIO_OType_PP,
-//            .GPIO_PuPd  = GPIO_PuPd_NOPULL,
-//        };
+    {
+        GPIO_InitTypeDef GPIO_InitStructure = {
+            .GPIO_Pin   = PULSE_GEN_MST_GPIO_PIN,
+            .GPIO_Mode  = GPIO_Mode_AF,
+            .GPIO_Speed = GPIO_Speed_100MHz,
+            .GPIO_OType = GPIO_OType_PP,
+            .GPIO_PuPd  = GPIO_PuPd_NOPULL,
+        };
 
-//        PULSE_GEN_GPIO_CLKEN(PULSE_GEN_GPIO_CLK, ENABLE);
-//        GPIO_PinAFConfig(PULSE_GEN_GPIO_PORT, PULSE_GEN_GPIO_PINSRC, PULSE_GEN_GPIO_AF);
-//        GPIO_Init(PULSE_GEN_GPIO_PORT, &GPIO_InitStructure);
-//    }
-//    // tim
-//    {
-//        PULSE_GEN_TIM_CLKEN(PULSE_GEN_MST_TIM_CLK, ENABLE);
-//        PULSE_GEN_TIM_CLKEN(PULSE_GEN_SLV_TIM_CLK, ENABLE);
-//    }
+        PULSE_GEN_MST_GPIO_CLKEN(PULSE_GEN_MST_GPIO_CLK, ENABLE);
+        GPIO_PinAFConfig(PULSE_GEN_MST_GPIO_PORT, PULSE_GEN_MST_GPIO_PINSRC, PULSE_GEN_MST_GPIO_AF);
+        GPIO_Init(PULSE_GEN_MST_GPIO_PORT, &GPIO_InitStructure);
+    }
+    // mst tim
+    {
+        TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+        TIM_OCInitTypeDef       TIM_OCInitStructure;
+
+        // clkin
+        PULSE_GEN_MST_TIM_CLKEN(PULSE_GEN_MST_TIM_CLK, ENABLE);
+        TIM_InternalClockConfig(PULSE_GEN_MST_TIM_PORT);
+
+        // timer
+        TIM_TimeBaseStructure.TIM_Prescaler         = 84 - 1;
+        TIM_TimeBaseStructure.TIM_CounterMode       = TIM_CounterMode_Up;
+        TIM_TimeBaseStructure.TIM_Period            = 100 - 1;
+        TIM_TimeBaseStructure.TIM_ClockDivision     = TIM_CKD_DIV1;
+        TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+        TIM_TimeBaseInit(PULSE_GEN_MST_TIM_PORT, &TIM_TimeBaseStructure);
+        TIM_SetAutoreload(PULSE_GEN_MST_TIM_PORT, 0);
+
+        // event
+        TIM_SelectMasterSlaveMode(PULSE_GEN_MST_TIM_PORT, TIM_MasterSlaveMode_Enable);
+        TIM_SelectOutputTrigger(PULSE_GEN_MST_TIM_PORT, TIM_TRGOSource_Update);
+
+        // pwm
+        TIM_OCInitStructure.TIM_OCMode      = TIM_OCMode_PWM1;
+        TIM_OCInitStructure.TIM_Pulse       = (TIM_TimeBaseStructure.TIM_Period + 1) * 0.33 - 1;  // duty
+        TIM_OCInitStructure.TIM_OCPolarity  = TIM_OCPolarity_High;
+        TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+        PULSE_GEN_MST_TIM_OCInit(PULSE_GEN_MST_TIM_PORT, &TIM_OCInitStructure);
+        TIM_OC4FastConfig(PULSE_GEN_MST_TIM_PORT, TIM_OCFast_Disable);
+    }
+    // slv tim
+    {
+        TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+
+        // clkin
+        PULSE_GEN_SLV_TIM_CLKEN(PULSE_GEN_SLV_TIM_CLK, ENABLE);
+
+        // timer
+        TIM_TimeBaseStructure.TIM_Prescaler         = 0;
+        TIM_TimeBaseStructure.TIM_CounterMode       = TIM_CounterMode_Up;
+        TIM_TimeBaseStructure.TIM_Period            = 1024 - 1;
+        TIM_TimeBaseStructure.TIM_ClockDivision     = TIM_CKD_DIV1;
+        TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+        TIM_TimeBaseInit(PULSE_GEN_SLV_TIM_PORT, &TIM_TimeBaseStructure);
+        TIM_SetAutoreload(PULSE_GEN_SLV_TIM_PORT, 0);
+
+        // event
+        TIM_SelectSlaveMode(PULSE_GEN_SLV_TIM_PORT, TIM_SlaveMode_External1);
+        TIM_SelectInputTrigger(PULSE_GEN_SLV_TIM_PORT, PULSE_GEN_SLV_TIM_ITR);
+        TIM_SelectMasterSlaveMode(PULSE_GEN_SLV_TIM_PORT, TIM_MasterSlaveMode_Disable);
+        TIM_SelectOutputTrigger(PULSE_GEN_SLV_TIM_PORT, PULSE_GEN_SLV_TIM_TRGSRC);
+
+        // nvic
+        NVIC_InitTypeDef NVIC_InitStructure;
+        NVIC_InitStructure.NVIC_IRQChannel                   = PULSE_GEN_SLV_TIM_IRQn;
+        NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+        NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 0;
+        NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
+        NVIC_Init(&NVIC_InitStructure);
+    }
 }
 
 #if 0
--- 4.Examples/std/demos/modbus_dma/Core/src/main.c
+++ 4.Examples/std/demos/modbus_dma/Core/src/main.c
@@ -50,96 +50,8 @@ void usdk_preinit(void)
 
 //
 
-/***定时器1主模式***/
-void TIM1_config(u32 Cycle)
-{
-    GPIO_InitTypeDef        GPIO_InitStructure;
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-    TIM_OCInitTypeDef       TIM_OCInitStructure;
-
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
-
-    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_11;  // TIM1_CH4 PA11
-    // GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_AF_PP;  // 复用推挽输出
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource11, GPIO_AF_TIM1);
-
-    TIM_TimeBaseStructure.TIM_Period            = Cycle - 1;
-    TIM_TimeBaseStructure.TIM_Prescaler         = 84 - 1;              // 设置用来作为TIMx时钟频率除数的预分频值
-    TIM_TimeBaseStructure.TIM_ClockDivision     = TIM_CKD_DIV1;        // 设置时钟分割：TDTS= Tck_tim
-    TIM_TimeBaseStructure.TIM_CounterMode       = TIM_CounterMode_Up;  // TIM向上计数模式
-    TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;                   // 重复计数，一定要=0！！！
-    TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
-
-    TIM_OCInitStructure.TIM_OCMode      = TIM_OCMode_PWM1;         // 选择定时器模式：TIM脉冲宽度调制模式1
-    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;  // 比较输出使能
-    TIM_OCInitStructure.TIM_Pulse       = Cycle / 2 - 1;           // 设置待装入捕获寄存器的脉冲值
-    TIM_OCInitStructure.TIM_OCPolarity  = TIM_OCPolarity_Low;      // 输出极性
-
-    TIM_OC4Init(TIM1, &TIM_OCInitStructure);
-
-    TIM_SelectMasterSlaveMode(TIM1, TIM_MasterSlaveMode_Enable);
-    TIM_SelectOutputTrigger(TIM1, TIM_TRGOSource_Update);
-
-    TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
-    TIM_ARRPreloadConfig(TIM1, ENABLE);
-}
-/***定时器2从模式***/
-void TIM2_config(u32 PulseNum)
-{
-    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-    NVIC_InitTypeDef        NVIC_InitStructure;
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
-
-    TIM_TimeBaseStructure.TIM_Period        = PulseNum - 1;
-    TIM_TimeBaseStructure.TIM_Prescaler     = 0;
-    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-    TIM_TimeBaseStructure.TIM_CounterMode   = TIM_CounterMode_Up;
-    TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-
-    TIM_SelectInputTrigger(TIM2, TIM_TS_ITR0);
-    // TIM_InternalClockConfig(TIM3);
-    TIM2->SMCR |= 0x07;  // 设置从模式寄存器
-    // TIM_ITRxExternalClockConfig(TIM2, TIM_TS_ITR0);
-
-    // TIM_ARRPreloadConfig(TIM3, ENABLE);
-    TIM_ITConfig(TIM2, TIM_IT_Update, DISABLE);
-
-    // NVIC_PriorityGroupConfig(NVIC_PriorityGroup_3);
-    NVIC_InitStructure.NVIC_IRQChannel                   = TIM2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
-}
-void Pulse_output(u32 Cycle, u32 PulseNum)
-{
-    TIM2_config(PulseNum);
-    TIM_Cmd(TIM2, ENABLE);
-    TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
-    TIM1_config(Cycle);
-
-    TIM_Cmd(TIM1, ENABLE);
-    TIM_CtrlPWMOutputs(TIM1, ENABLE);  // 高级定时器一定要加上，主输出使能
-}
-
-void TIM2_IRQHandler(void)
-{
-    if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)  // TIM_IT_CC1
-    {
-        TIM_ClearITPendingBit(TIM2, TIM_IT_Update);  // 清除中断标志位
-        TIM_CtrlPWMOutputs(TIM1, DISABLE);           // 主输出使能
-        TIM_Cmd(TIM1, DISABLE);                      // 关闭定时器
-        TIM_Cmd(TIM2, DISABLE);                      // 关闭定时器
-        TIM_ITConfig(TIM2, TIM_IT_Update, DISABLE);
-    }
-}
-
-//
+void PulseGenInit(void);
+void PulseGenN(uint32_t count);
 
 int main()
 {
@@ -152,14 +64,12 @@ int main()
 
     eMBEnable();
 
-    // PulseGenInit();
     // PulseGenConfig(10000, 230);
+    PulseGenInit();
 
     WaveGenInit();
 
-    Pulse_output(1000, 8000);  // 1KHZ，8000个脉冲
-
-    P(DrvCfg).u16BlinkPeriod = 500;
+    P(DrvCfg).u16BlinkPeriod = 1000;
 
     while (1)
     {
@@ -181,6 +91,7 @@ int main()
         if (DelayNonBlockMS(t_blink, P(DrvCfg).u16BlinkPeriod))
         {
             PEout(15) ^= 1;  // toggle
+            PulseGenN(8);
             t_blink = HAL_GetTick();
         }
 #endif
